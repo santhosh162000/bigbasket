f3ebf156c9193c0d2a2df3d4b7035607825afe09
santhosh162000 <santhosh.m@codingmart.com>
2021-03-21 18:24:27 +0530

    text in images

--- src/components/SecondSlide.js
+++ src/components/SecondSlide.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from "styled-components";
 import Carosal from "./Carosal";
-
+import './SecondSlide.css';
 import ImageOne from "../assets/SecondCarosal/2.png"
 
 export default function FirstSlider() {
@@ -9,7 +9,10 @@ export default function FirstSlider() {
     return (
         <SecondSlideContainer>
             <span>
+            <div class="container">
             <Carosal ImageArr={ImageArr} />
+            <div class="centered"><p className="the">The</p><p className="hol">Holi Special</p><h6 className="ho">Home & Kitchen Sale</h6></div>
+            </div>
             </span>
         </SecondSlideContainer>
     )
